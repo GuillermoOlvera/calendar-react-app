6fc4bdd9aee1539fac118e7e7781c537fa85ba08
Guillermo <memolvpz@gmail.com>
2023-04-17 19:30:29 -0600

    startDeletingNotes created with FabDelete component

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux"
-import { onAddNewEvent, onSetActiveEvent, onUpdateEvent } from "../store";
+import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent } from "../store";
 
 export const useCalendarStore = () => {
 
@@ -19,14 +19,20 @@ export const useCalendarStore = () => {
         }
     }
 
+    const startDeletingEvent = () => {
+        dispatch(onDeleteEvent());
+    }
+
     return {
 
         // Properties
         events,
         activeEvent,
+        hasEventSelected: !!activeEvent,
 
         // Methods
         setActiveEvent,
         startSavingEvent,
+        startDeletingEvent,
     }
 }
