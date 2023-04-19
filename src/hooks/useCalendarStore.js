f780e75ba4f820ed6e4841b30d0d9a74c84a4456
Guillermo Olvera López <73192041+GuillermoOlvera@users.noreply.github.com>
2023-04-18 20:50:11 -0600

    creating events in calendar

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,21 +1,26 @@
 import { useDispatch, useSelector } from "react-redux"
 import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent } from "../store";
+import calendarApi from "../api/calendarApi";
 
 export const useCalendarStore = () => {
 
     const dispatch = useDispatch();
     const { events, activeEvent } = useSelector(state => state.calendar);
+    const { user } = useSelector(state => state.auth);
 
     const setActiveEvent = (calendarEvent) => {
         dispatch(onSetActiveEvent(calendarEvent))
     }
 
     const startSavingEvent = async( calendarEvent ) => {
+
         if(calendarEvent._id) {
             dispatch(onUpdateEvent({ ...calendarEvent }));
         } else {
             // creating
-            dispatch(onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() }))
+            const { data } = await calendarApi.post('/events', calendarEvent)
+            console.log({data});
+            dispatch(onAddNewEvent({ ...calendarEvent, id: data.evento.id, user }))
         }
     }
 
